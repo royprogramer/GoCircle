73f80f051ef0aa2bc7043fc68afa6316ead2c3ca
arunimamithila <arunimamithila@gmail.com>
2023-03-03 03:06:06 +0600

    testing

---
+++ src/main/java/com/teamvoid/gocircle/LoginController.java
@@ -0,0 +1,22 @@
+package com.teamvoid.gocircle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    void loginButton(ActionEvent event) {
+
+    }
+
+}
+
