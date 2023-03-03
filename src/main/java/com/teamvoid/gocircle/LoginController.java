7c60183356098dd7d1e1c96696ee551adbef462a
arunimamithila <arunimamithila@gmail.com>
2023-03-03 12:29:08 +0600

    new scene

--- src/main/java/com/teamvoid/gocircle/LoginController.java
+++ src/main/java/com/teamvoid/gocircle/LoginController.java
@@ -2,10 +2,17 @@ package com.teamvoid.gocircle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,6 +28,16 @@ public class LoginController implements Initializable {
     @FXML
     void loginButton(ActionEvent event) {
 
+    }
+    @FXML
+    void signupLable(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/register.fxml"));
+        Scene scene2 = new Scene(root);
+
+        Stage stage2 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        stage2.setScene(scene2);
+        stage2.show();
     }
 
     @Override
