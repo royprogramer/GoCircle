356b171860fb7a508fbda336a9db684ac03ae069
Koushik Roy <sampadroykoushik01@gmail.com>
2023-04-12 03:05:12 +0600

    module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,15 @@
 module com.example.gocircle {
     requires javafx.controls;
     requires javafx.fxml;
-    requires  java.sql;
+    requires io.github.cdimascio.dotenv.java;
+    requires java.mail;
+    requires java.sql;
 
 
     opens com.teamvoid.gocircle to javafx.fxml;
     exports com.teamvoid.gocircle;
+    exports com.teamvoid.gocircle.Server;
+    opens com.teamvoid.gocircle.Server to javafx.fxml;
+    exports com.teamvoid.gocircle.Client;
+    opens com.teamvoid.gocircle.Client to javafx.fxml;
 }
