e67ae9244896f8ee03b0b21e7fb34b1d7f16c371
arunimamithila <arunimamithila@gamil.com>
2023-04-28 21:32:33 +0600

    newui

---
+++ src/main/java/com/teamvoid/gocircle/FirstpageController.java
@@ -0,0 +1,76 @@
+package com.teamvoid.gocircle;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FirstpageController implements Initializable{
+    @FXML
+    private AnchorPane rootpane;
+    @FXML
+    private ImageView circlegif;
+    @FXML
+    void close(MouseEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void minimize(MouseEvent event) {
+        Main.stage.setIconified(true);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)  {
+       new SplashScreen().start();
+
+    }
+    class SplashScreen extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(21000);
+
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        Parent root = null;
+                        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/dfg.fxml"));
+                        Scene scene = null;
+                        try {
+                            scene = new Scene(fxmlLoader.load());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        Stage stage = new Stage();
+
+                        stage.setScene(scene);
+                        stage.initStyle(StageStyle.UNDECORATED);
+                        stage.show();
+
+                        rootpane.getScene().getWindow().hide();
+                    }
+                });
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.gocircle {
     requires io.github.cdimascio.dotenv.java;
     requires java.mail;
     requires java.sql;
+    requires com.jfoenix;
 
 
     opens com.teamvoid.gocircle to javafx.fxml;
